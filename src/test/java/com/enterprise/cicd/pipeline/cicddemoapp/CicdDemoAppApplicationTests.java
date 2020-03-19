897d4e3ab5b86cc98de455231ba5f93ce467d124
Leo Jose <leo.jose@siemens.com>
2020-03-19 11:12:14 +0530

    first commit

---
+++ src/test/java/com/enterprise/cicd/pipeline/cicddemoapp/CicdDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.cicd.pipeline.cicddemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
